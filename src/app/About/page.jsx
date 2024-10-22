9e941d6b8377c94f070dca3112999189f36a2d7d
Harshit Raj <rajharshit634@gmail.com>
2024-10-22 12:07:03 +0530

    fix different pages issues

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -1,9 +1,8 @@
 export default function About() {
   return (
     <div className="pt-16">
-      {" "}
       {/* Add padding to push content down */}
-      <p className="text-violet-700 text-center mt-20 font-bold mb-80 mt-80 text-3xl">
+      <p className="text-violet-700 text-center mt-80 font-bold mb-80 text-3xl">
         Coming Soon..........
       </p>
     </div>
